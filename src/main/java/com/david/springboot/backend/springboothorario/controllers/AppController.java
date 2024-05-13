f7c9f4e51fc0d60486a936118aba00f0cf8b756d
Alex David Frías <adavidfruiz@gmail.com>
2024-05-13 01:41:31 -0500

    feat: add AppController

--- src/main/java/com/david/springboot/backend/springboothorario/controllers/AppController.java
+++ src/main/java/com/david/springboot/backend/springboothorario/controllers/AppController.java
@@ -1,9 +1,21 @@
 package com.david.springboot.backend.springboothorario.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AppController {
-
+@GetMapping("/foo")
+    public ResponseEntity<?> foo() {
+    Map<String, Object> data =new HashMap<>();
+    data.put("title", "Bienvenidos al sistema de atención");
+    data.put("time", new Date());
+    return ResponseEntity.ok(data);
+    }
 
 }
