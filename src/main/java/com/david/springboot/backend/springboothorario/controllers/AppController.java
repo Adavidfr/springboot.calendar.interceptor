b556a6957e2efe578b4d0f3976054c4217b56657
Alex David Frías <adavidfruiz@gmail.com>
2024-05-13 01:28:52 -0500

    feat: add

---
+++ src/main/java/com/david/springboot/backend/springboothorario/controllers/AppController.java
@@ -0,0 +1,9 @@
+package com.david.springboot.backend.springboothorario.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+
+}
