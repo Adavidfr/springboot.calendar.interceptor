86d9a5d4d06dcc53d673c9adf394c72509a85316
Alex David Frías <adavidfruiz@gmail.com>
2024-05-14 01:58:30 -0500

    feat: add http request

--- src/main/java/com/david/springboot/backend/springboothorario/controllers/AppController.java
+++ src/main/java/com/david/springboot/backend/springboothorario/controllers/AppController.java
@@ -1,5 +1,6 @@
 package com.david.springboot.backend.springboothorario.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,11 @@ import java.util.Map;
 @RestController
 public class AppController {
 @GetMapping("/foo")
-    public ResponseEntity<?> foo() {
+    public ResponseEntity<?> foo(HttpServletRequest request) {
     Map<String, Object> data =new HashMap<>();
     data.put("title", "Bienvenidos al sistema de atención");
     data.put("time", new Date());
+    data.put("message", request.getAttribute("message"));
     return ResponseEntity.ok(data);
     }
 
