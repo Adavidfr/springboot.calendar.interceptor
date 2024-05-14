1da83f0141cd76ae23d7f57302742fad6c17ad06
Alex David Frías <adavidfruiz@gmail.com>
2024-05-14 02:00:03 -0500

    feat: add calendar interceptor class

---
+++ src/main/java/com/david/springboot/backend/springboothorario/interceptors/CalendarInterceptor.java
@@ -0,0 +1,54 @@
+package com.david.springboot.backend.springboothorario.interceptors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CalendarInterceptor implements HandlerInterceptor {
+
+    @Value("${config.calendar.open}")
+    private Integer open;
+
+    @Value("${config.calendar.close}")
+    private Integer close;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        System.out.println(hour);
+
+        if (hour >= open && hour < close) {
+            StringBuilder message = new StringBuilder("Bienvenidos al horario de atención a clientes, atendemos desde las "
+                    + open + " hrs. hasta las " + close + " hrs. Gracias por su visita!");
+            request.setAttribute("message", message.toString());
+            return true;
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> data = new HashMap<>();
+        StringBuilder message = new StringBuilder("Cerrado, fuera del horario de atención, por favor visitenos desde las "
+                + open + " hrs. hasta las " + close + " hrs. Gracias!");
+        data.put("message", message.toString());
+        data.put("date", new Date());
+        response.setContentType("application/json");
+        response.setStatus(401);
+        response.getWriter().write(mapper.writeValueAsString(data));
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+}
